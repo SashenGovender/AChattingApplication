bb52864c700fcc80a2b8275e4130ed2306c98c12
Sashen <sashen50@gmail.com>
2020-04-12 00:09:32 +0200

    completed css and html for javscript client

---
+++ ChattingServer/wwwroot/js/homepagesetup.js
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChattingServer.wwwroot.js
+{
+    public class homepagesetup
+    {
+    }
+}

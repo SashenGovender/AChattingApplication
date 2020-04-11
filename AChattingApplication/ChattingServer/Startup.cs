b98b4a48fc80ee58b5ca78b0697aa7eb4fe46c50
Sashen <sashen50@gmail.com>
2020-04-11 16:45:10 +0200

    Added SignalR ConfigureService and route

--- AChattingApplication/ChattingServer/Startup.cs
+++ AChattingApplication/ChattingServer/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChattingServer.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,7 +24,9 @@ namespace ChattingServer
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddRazorPages();
+      services.AddSignalR();
+
+      //services.AddSingleton<ChatTracker>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,11 +48,11 @@ namespace ChattingServer
 
       app.UseRouting();
 
-      app.UseAuthorization();
+      app.UseAuthentication();
 
       app.UseEndpoints(endpoints =>
       {
-        endpoints.MapRazorPages();
+        endpoints.MapHub<ChatRoomHub>("/chatroom");
       });
     }
   }

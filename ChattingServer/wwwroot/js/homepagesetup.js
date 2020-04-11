454112a512234728d1f45492395bea8a8f729f0b
Sashen <sashen50@gmail.com>
2020-04-12 00:19:00 +0200

    moved html script js to seperate js files

--- ChattingServer/wwwroot/js/homepagesetup.js
+++ ChattingServer/wwwroot/js/homepagesetup.js
@@ -1,11 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//Modal Logic
 
-namespace ChattingServer.wwwroot.js
-{
-    public class homepagesetup
-    {
+//Display a modal for the user to set their username
+function DisplayModal() {
+
+    var modal = document.getElementById("userNameModelId"); // Get the modal
+    modal.style.display = "block"; //Display the model
+
+    var span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
     }
 }
+
+function CompleteSetup() {
+    // remove the model
+    var modal = document.getElementById("userNameModelId");
+    modal.style.display = "none";
+
+    var userName = (document.getElementById("userNameInputId")).value;
+
+    //set username at top left
+    var userNameLabel = (document.getElementById("userNameLabelId"));
+    userNameLabel.innerHTML = userName;
+}
+
+// Buttons
+function StartConnection() {
+
+}
+
+function CloseConnection() {
+
+}
+
+function SendMessage() {
+
+}
+
+//document.addEventListener('DOMContentLoaded', ready);
+setTimeout(DisplayModal, 50);
